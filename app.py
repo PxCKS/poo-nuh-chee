e00e4cbc44a38471770cd533508fa44d35bde9c1
SivuyiseNcapayi <sivuyisencapai@gmail.com>
2024-07-15 15:29:17 +0200

    Frontend: Initial Design

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request
-import config
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
-    username = request.form.get('username')
-    post_content = request.form.get('content')
-
 
     return render_template('index.html')
 
